5d676f13c00d07bce79a7abb5d9bdabf9bdc44fd
andrewicham <andrewicham@gmail.com>
2019-02-23 17:07:43 -0500

    added 4 functions which sets the array size, populates the array with random values, checks to see if array is sorted, and prints the array

--- sortingApp.c
+++ sortingApp.c
@@ -1,38 +1,76 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-/*example from textbook, multithreaded C program using Pthreads API*/
 
-int sum; /* this data is shared by the thread(s) */
-void *runner(void *param); /* threads call this function */
+//declaring the randArray dynamically so that a user defined length can be determined
+int* randArray = NULL;
+
+void populateArray(int* randArray, unsigned int size);
+void printArray(int* randArray, unsigned int size);
+void setArraySize(unsigned int size);
+void checkIfSorted(int* randArray, unsigned int size);
 
 int main(int argc, char *argv[])
 {
-    pthread_t tid; /* the thread identifier */
-    pthread_attr_t attr; /* set of thread attributes */
-    if (argc != 2) {
-        fprintf(stderr,"usage: a.out <integer value>\n");
-        return -1;
-    }
-    if (atoi(argv[1]) < 0) {  
-        fprintf(stderr,"%d must be >= 0\n",atoi(argv[1]));
-        return -1;
-    }
-    /* get the default attributes */
-    pthread_attr_init(&attr);
-    /* create the thread */
-    pthread_create(&tid,&attr,runner,argv[1]);
-    /* wait for the thread to exit */
-    pthread_join(tid,NULL);
-    printf("sum = %d\n",sum);
-}
-/* The thread will begin control in this function */
-void *runner(void *param)
-{
-    int i, upper = atoi(param);
-    sum = 0;
-    for (i = 1; i <= upper; i++)
-    sum += i;
-    pthread_exit(0);
+    unsigned int size = 0;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    printf("\n");
+
+    setArraySize(size);
+    populateArray(randArray, size);
+
+    printArray(randArray, size);
+    checkIfSorted(randArray, size);
+
+    free(randArray); //frees the dynamically allocated memory for randArray
+    return 0;
 }
 
+/* Sets randArray to be a user determined size */
+void setArraySize(unsigned int size){
+    if(!randArray){
+        randArray = malloc(size);
+    }
+}
+
+/*this function will populate randArray with randomly generated numbers using the current time as a seed*/
+void populateArray(int* randArray, unsigned int size){
+    int i;
+    //the current time used as a seed for the random number generator
+    srand(time(0));
+    for(i = 0; i < size; i++){
+        randArray[i] = rand();
+    }
+}
+
+/*this function is used to test if the array has been sorted properly*/
+void printArray(int* randArray, unsigned int size){
+    int i;
+    for(i = 0; i < size; i++){
+        printf("Array index: %d\nArray Value: %d\n\n", i, randArray[i]);
+    }
+}
+
+/* This function checks to see if the array is properly sorted */
+void checkIfSorted(int* randArray, unsigned int size){
+    int isSorted;
+    int i;
+    for(i = 0; i < size-1; i++){
+        if(randArray[i] <= randArray[i+1]){
+            isSorted = 1;
+        }
+        else{
+            isSorted = 0;
+            break;
+        }
+    }
+    if(isSorted == 1){
+        printf("The array is sorted\n");
+    }
+    else{
+        printf("Array is not sorted\n");
+    }
+}

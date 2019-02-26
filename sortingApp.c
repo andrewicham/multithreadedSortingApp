cfdc1aefed3e344e97d420fd45579b7321ab66f8
andrewicham <andrewicham@gmail.com>
2019-02-26 12:35:10 -0500

    fixed segmentation fault with swap and partition algorithms

--- sortingApp.c
+++ sortingApp.c
@@ -11,11 +11,13 @@ int* randArray = NULL;
 //function declarations
 void setArraySize(unsigned int size);
 void populateArray(int* randArray, unsigned int size);
-void printArray(int* randArray, unsigned int size);
+void printArray(int randArray[], unsigned int size);
 void checkIfSorted(int* randArray, unsigned int size);
 void *runner(void* param);
 int partition(int* randArray, int low, int high);
-void quickSort(int* randArray, int low, int high);
+void quickSort(int* randArra, int low, int high);
+void swap(int arr[], int a, int b);
+
 
 /* Sets randArray to be a user determined size */
 void setArraySize(unsigned int size){
@@ -30,7 +32,7 @@ void populateArray(int* randArray, unsigned int size){
     //current time used as the seed for the random number generator
     srand(time(0));
     for(i = 0; i < size; i++){
-        randArray[i] = rand() / 100000;
+        randArray[i] = rand() / 10000;
     }
 }
 
@@ -65,61 +67,89 @@ void checkIfSorted(int* randArray, unsigned int size){
 
 void *runner(void *param){
     int i, upper = atoi(param);
-
-
-
-
-
+    
+    
+    
+    
+    
     pthread_exit(0);
 }
 
-void quickSort(int* randArray, int low, int high){
-    int pIndex;
+void quickSort(int *randArray, int low, int high){
     if(low < high){
-        pIndex = partition(randArray, low, high);
+        int pIndex = partition(randArray, low, high);
         quickSort(randArray, low, pIndex-1);
         quickSort(randArray, pIndex + 1, high);
     }
 }
 
-int partition(int* randArray, int low, int high){
+int partition(int *randArray, int low, int high){
     int pivotPoint = randArray[high];
     int i, j;
-    int temp;
     i = low - 1;
-
     for(j = low; j <= high; j++){
         if(randArray[j] <= pivotPoint){
             i++;
-            temp = randArray[i];
-            randArray[i] = randArray[j];
-            randArray[j] = temp;
+            swap(randArray, i, j);
         }
     }
-    temp = randArray[i+1];
-    randArray[i+1] = randArray[high];
-    randArray[high] = temp;
-    return (i + 1);
+    swap(randArray, (i+1), high);
+    return (i+1);
+}
+void swap(int arr[], int a, int b) {
+    int temp;
+    temp = arr[b];
+    arr[b] = arr[a];
+    arr[a] = temp;
     
 }
 
 int main(int argc, char *argv[])
 {
     unsigned int size = 0;
-    
+    /*if(argc != 2){
+     fprintf(stderr, "usage: a.out <integer value>\n");
+     return -1;
+     }
+     if(atoi(argv[1]) < 0){
+     fprintf(stderr,"%d must be >= 0\n",atoi(argv[1]));
+     return -1;
+     }*/
     
     printf("Enter the size of the array to generate: ");
     scanf("%d", &size);
     printf("\n");
+
     setArraySize(size);
     populateArray(randArray, size);
-
-    //checkIfSorted(randArray, size);
     
+    checkIfSorted(randArray, size);
     printArray(randArray, size);
-    
+
     quickSort(randArray, 0, size - 1);
     
+    printArray(randArray, size);
+    checkIfSorted(randArray, size);
+    
+    //starts the clock for tread creation
+    //clock_t start = clock();
+    
+    /*
+     pthread_t tid;
+     pthread_attr_t attr;
+     
+     pthread_t tid2;
+     pthread_attr_t attr2;
+     
+     pthread_t tid3;
+     pthread_attr_t attr3;
+     
+     pthread_attr_init(&attr);
+     pthread_create(&tid, &attr, );*/
+    
+    //stops clock after threads have completed running
+    //clock_t stop = clock();
+    //printf("Elapsed time: %f seconds\n", (double)(stop - start) / CLOCKS_PER_SEC);
     free(randArray); //frees the dynamically allocated memory for randArray
     return 0;
 }
